fbc987b81d45e039fb57d5962fa8467a2592cf70
vitordiamantino <vitorfdiamantino@gmail.com>
2026-09-14 20:42:28 -0300

    adc funcionamento de funcionais

---
+++ JAVA/MetodosObjetos/ProgramacaoFuncional/src/exemplo1/Main.java
@@ -0,0 +1,8 @@
+package exemplo1;
+
+public class Main {
+    public static void main(String[] args) {
+        Professor p = new Professor(1, "A");
+        System.out.println(p);
+    }
+}
---
+++ JAVA/MetodosObjetos/ProgramacaoFuncional/src/exemplo1/Professor.java
@@ -0,0 +1,6 @@
+package exemplo1;
+
+public record Professor(int cpf, String nome) {
+}
+
+
